ad4d7227355bd14c6e69f96dad4d41710799aa86
patelsagar1712 <patelsagar1712@users.noreply.github.com>
2020-12-28 12:19:50 +0530

    Removed Genric css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './components/Header/Header';
 
 function App(): JSX.Element {
-	return <div className='App'>{<Header userName='' isLoggedIn={false} itemsInCart={1} theme='white' />}</div>;
+  return <div className='App'>{<Header userName='' isLoggedIn={false} itemsInCart={1} theme='white' />}</div>;
 }
 
 export default App;
