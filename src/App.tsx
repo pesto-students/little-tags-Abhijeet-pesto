20e6535a96ae0ef74e88b52b7fd18bb88c15d3ef
Abhijeet Agarwal <abhijeet.agarwal23@gmail.com>
2020-12-30 22:02:49 +0530

    Added initail routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Header } from './components/Header/Header';
-import { ViewAllCategory } from './routes';
+import { Home, Cart, ViewAllCategory, Product, Orders } from './routes';
 import { Layout } from './components';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App(): JSX.Element {
 	return (
 		<div className='App'>
 			<Layout isLoggedIn={false} userName={''} itemsInCart={0} theme='dark'>
-				<ViewAllCategory />
+				<Router>
+					<Switch>
+						<Route path='/orders'>
+							<Orders />
+						</Route>
+						<Route path='/cart'>
+							<Cart />
+						</Route>
+						<Route path='/product'>
+							<Product />
+						</Route>
+						<Route path='/viewAll'>
+							<ViewAllCategory />
+						</Route>
+						<Route path='/'>
+							<Home />
+						</Route>
+					</Switch>
+				</Router>
 			</Layout>
 		</div>
 	);
