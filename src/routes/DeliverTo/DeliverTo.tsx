f3335e6a1020b0374dc57c95982d949108c772b1
patelsagar1712 <patelsagar1712@users.noreply.github.com>
2020-12-30 21:52:03 +0530

    Added missing Files

---
+++ src/routes/DeliverTo/DeliverTo.tsx
@@ -0,0 +1,44 @@
+import { ReactElement } from 'react';
+import './DeliverTo.css';
+import * as GrIcons from 'react-icons/gr';
+import { DeliveryCard } from '../../components/common';
+
+export const DeliverTo = (): ReactElement => {
+	return (
+		<div className='container'>
+			<div className='header'>Deliver To</div>
+
+			<DeliveryCard
+				onChange={() => undefined}
+				name='Add'
+				value='Address'
+				label={
+					<div>
+						<div className='name'> Sagar Patel</div>
+						<div className='address'>1418 Riverwood Drive, Suite 3245 Cottonwood, DL 110092, India </div>
+					</div>
+				}
+			/>
+			<DeliveryCard
+				onChange={() => undefined}
+				name='Add'
+				value='Address'
+				label={
+					<div>
+						<div className='name'> Sagar Patel</div>
+						<div className='address'> New York, </div>
+					</div>
+				}
+			/>
+
+			<button className='addNewAdd'>
+				<GrIcons.GrAddCircle />
+				Add Address
+			</button>
+
+			<div className='proceedBtnPadding'>
+				<button className='proceedButton'>Proceed</button>
+			</div>
+		</div>
+	);
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,3 +2,5 @@ export { Home } from './Home/Home';
 export { ViewAllCategory } from './ViewAllCategory/ViewAllCategory';
 export { Cart } from './Cart/Cart';
 export { Product } from './Product/Product';
+export { DeliverTo } from './DeliverTo/DeliverTo';
+export { PaymentPage } from './PaymentPage/PaymentPage';
