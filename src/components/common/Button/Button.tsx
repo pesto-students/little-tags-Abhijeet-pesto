250562c9c9be456ed52bc544f920d14888c441f9
Abhijeet Agarwal <abhijeet.agarwal23@gmail.com>
2021-01-04 15:24:55 +0530

    Added disabled prop in Button component Added checked prop in Radio Component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,19 +1,26 @@
 import { ReactElement, MouseEvent } from 'react';
 import './Button.css';
 
-interface ButtonProps {
+const defaultProps = {
+	disabled: false,
+};
+
+type ButtonProps = {
 	type: 'button' | 'submit';
 	children: string;
+	disabled: boolean;
 	renderIcon?: ReactElement;
 	onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 	className?: string;
-}
+} & typeof defaultProps;
 
-export const Button = ({ type, onClick, className, renderIcon, children }: ButtonProps): ReactElement => {
+export const Button = ({ type, onClick, className, renderIcon, disabled, children }: ButtonProps): ReactElement => {
 	return (
-		<button type={type} onClick={onClick} className={`btn little-tags-btn ${className}`}>
+		<button type={type} onClick={onClick} className={`btn little-tags-btn ${className}`} disabled={disabled}>
 			{renderIcon && <div className='button-icon-container'>{renderIcon}</div>}
 			<div className='button-text-container'>{children}</div>
 		</button>
 	);
 };
+
+Button.defaultProps = defaultProps;
--- src/components/common/RadioInput/RadioInput.tsx
+++ src/components/common/RadioInput/RadioInput.tsx
@@ -5,14 +5,15 @@ interface RadioInputProps {
 	name: string;
 	label: ReactElement;
 	value: string | number;
+	checked: boolean;
 	onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const RadioInput = ({ name, label, value, onChange }: RadioInputProps): ReactElement => {
+export const RadioInput = ({ name, label, value, checked, onChange }: RadioInputProps): ReactElement => {
 	return (
 		<label className='radio'>
 			<span className='radio-input'>
-				<input type='radio' name={name} onChange={onChange} value={value} />
+				<input type='radio' name={name} onChange={onChange} value={value} checked={checked} />
 				<span className='radio-control'></span>
 			</span>
 			<div className='radio-label'>{label}</div>
