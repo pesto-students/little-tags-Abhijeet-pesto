331f2b2058f5d5b3ca7a30fdb4ac7a9f51625d3d
patelsagar1712 <patelsagar1712@users.noreply.github.com>
2021-01-04 19:59:18 +0530

    Updated Sidebar

--- src/components/ProductDesc/ProductDesc.tsx
+++ src/components/ProductDesc/ProductDesc.tsx
@@ -24,13 +24,8 @@ interface ProductDescProps {
 	onQuantityChange: (newQuantity: number) => void;
 	onSizeChange: (size: SIZE) => void;
 }
-
-const images = [
-	'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
-	'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
-	'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
-	'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80',
-];
+const publicUrl = process.env.PUBLIC_URL;
+const images = [publicUrl + '/1_1.jpg', publicUrl + '/1_2.jpg', publicUrl + '/1_3.jpg', publicUrl + '/1_4.jpg'];
 
 const getSizes = () => Object.values(SIZE);
 
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -7,19 +7,39 @@ import { useDispatch } from 'react-redux';
 import { logOut } from '../../slices';
 import * as BiIcons from 'react-icons/bi';
 import * as RiIcons from 'react-icons/ri';
+import { setInventoryFilter } from '../../slices';
 
 interface Props {
 	sidebar: boolean;
 	isLoggedIn: boolean;
 	userName: string | null;
 }
+interface itemObj {
+	title: string | undefined;
+	path: string;
+	cName: string | undefined;
+	icon: string | undefined;
+	category: string;
+}
 
 export const SideBar = ({ sidebar, isLoggedIn, userName }: Props): ReactElement => {
 	const [sidebarComponent, setSidebarComponent] = useState(sidebar);
 	const history = useHistory();
 	const dispatch = useDispatch();
-	const onClickHistory = (item: string) => {
-		history.push(item);
+	const onClickHistory = (item: itemObj) => {
+		const category = item.category;
+		if (category?.length === 0) history.push(item.path);
+		else {
+			dispatch(
+				setInventoryFilter({
+					filter: {
+						filterBy: 'category',
+						filterValue: item.category,
+					},
+				}),
+			);
+			history.push(`/viewall`);
+		}
 	};
 	const onLogout = () => {
 		console.log('here');
@@ -29,13 +49,13 @@ export const SideBar = ({ sidebar, isLoggedIn, userName }: Props): ReactElement 
 	return (
 		<nav className={sidebarComponent ? 'nav-menu active' : 'nav-menu'}>
 			<ul className='nav-menu-items' onClick={showSidebarComponent}>
-				<li className='navbar-toggle'>
+				<li className='navbar-toggle' key='header'>
 					<span className='menu-bars'>
 						<AiIcons.AiFillCloseCircle color='black' size={20}></AiIcons.AiFillCloseCircle>
 					</span>
 					<span className='nav-header'>Little Tags</span>
 				</li>
-				<li>
+				<li key='userName'>
 					<span className='nav-text nav-user'>
 						<BiIcons.BiUserCircle size={30} />
 						<span>
@@ -47,7 +67,7 @@ export const SideBar = ({ sidebar, isLoggedIn, userName }: Props): ReactElement 
 				{SidebarData.map((item, index) => {
 					return item.title !== '' ? (
 						<li key={index} className={item.cName}>
-							<span onClick={() => onClickHistory(item.path)}>
+							<span onClick={() => onClickHistory(item)}>
 								{item.icon}
 								<span>{item.title}</span>
 							</span>
@@ -56,7 +76,7 @@ export const SideBar = ({ sidebar, isLoggedIn, userName }: Props): ReactElement 
 						<hr></hr>
 					);
 				})}
-				<li>
+				<li key='logoutBtn'>
 					<button className='nav-text logout' onClick={onLogout}>
 						<RiIcons.RiLogoutCircleFill color='red' size={20} />
 						Logout
